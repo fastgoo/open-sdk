202f769294facc359bc0e46ae4a8a48b89252a09
周先生 <Mr.Zhou@zhouxianshengdeMacBook-Pro.local>
2018-03-29 14:26:54 +0800

    添加微信授权范例

--- AuthExample.php
+++ AuthExample.php
@@ -11,7 +11,7 @@ include "./vendor/autoload.php";
 use FastGoo\WechatAuth;
 
 $type = !empty($_GET['demo_type']) ? $_GET['demo_type'] : 'decode';
-$config = ['app_id' => '', 'app_secret' => ''];
+$config = ['app_id' => '5a5bffe6dedb3', 'app_secret' => '86b1c50e6c5e068ae7d7b465d6758b8c'];
 $wechatAuth = new WechatAuth($config);
 switch ($type) {
     case 'decode':
